d6cf1aa4a84f8382a10fd60a33c4b7e855107e44
unknown <pedrotimoteocosta@gmail.com>
2017-10-20 21:56:58 -0200

    loading animations

--- access-manager/src/app/app.component.ts
+++ access-manager/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Event, NavigationStart, NavigationEnd, NavigationCancel, NavigationError, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  public loading = true;
+
+  constructor(private router: Router) {
+    router.events.subscribe((routerEvent: Event) => {
+        this.checkRouterEvent(routerEvent);
+    });
+  }
+
+  checkRouterEvent(routerEvent: Event): void {
+      if (routerEvent instanceof NavigationStart) {
+          this.loading = true;
+      }
+
+      if (routerEvent instanceof NavigationEnd ||
+          routerEvent instanceof NavigationCancel ||
+          routerEvent instanceof NavigationError) {
+          this.loading = false;
+      }
+  }
 }
